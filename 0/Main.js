0e720117556509fbfe9d4807bb0eaeb1f0f70b83
jackb0back <54455407+jackb0back@users.noreply.github.com>
2022-11-06 15:10:47 -0500

    Create Main.js

---
+++ 0/Main.js
@@ -0,0 +1,7 @@
+var Items;
+//for a google drive item
+//Items.push({Name: "", src: "", oexe: "", type: "Drive"}) SRC AND OEXE IS THE DRIVE FILE ID
+//Items.push({ Name: "DriveTest", src: "1ev0SnAgaIcVsWrkmCDVnS4xwpSgO2hmH", oexe: "1ev0SnAgaIcVsWrkmCDVnS4xwpSgO2hmH", type: "Drive"});
+Items.push({ Name: "SuperMario64 web", src: "1Sp8mtZVoeHJrb1DArdhRt2A-vRp4Ymn9", oexe: "1Sp8mtZVoeHJrb1DArdhRt2A-vRp4Ymn9", type: "Drive"});
+Items.push({ Name: "OASIS GBA", src: "195TCVk_v3LNL8qrWMz0VUqk8LhVapF29", oexe: "195TCVk_v3LNL8qrWMz0VUqk8LhVapF29", type: "Drive"});
+Items.push({ Name: "Bad time simulator", src: "1yv5CexFYvG8e0EDuer1CynhH22QlVg5k", oexe: "1yv5CexFYvG8e0EDuer1CynhH22QlVg5k", type: "Drive"});
